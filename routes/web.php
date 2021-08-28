4651efdf9f58f9de52cd30e3568c76dead3c306f
mikekolas <michalsavv@gmail.com>
2021-08-28 17:44:23 +0300

    Heroku task status fix attempt

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::resource('/lists', ToDoListController::class);
 Route::resource('/tasks', TaskController::class);
 Route::get('/tasks/{id}/status/{status}', 'App\Http\Controllers\TaskController@updateStatus')
     ->name('tasks.updateStatus')
-    ->where(['id' => '[0-9]+', 'status' => '[0-1]']);
+    ->where(['id' => '[0-9]+', 'status' => '^(true|false)$|[0-1]']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
